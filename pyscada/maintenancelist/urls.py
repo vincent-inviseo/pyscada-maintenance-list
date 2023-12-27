7c6ece59dbf86317f1725266a7f9b405825f1183
Alexandre Maurice <hizaaknewton@gmail.com>
2023-12-27 13:53:15 +0100

    Changement de la route

--- pyscada/maintenancelist/urls.py
+++ pyscada/maintenancelist/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("maintenancelist/device/<device_id>", views.device_informations, name="device-informations"),
+    path("view/<link_title>/device/<device_id>", views.device_informations, name="device-informations"),
 ]
--- pyscada/maintenancelist/views.py
+++ pyscada/maintenancelist/views.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.shortcuts import redirect
 from django.template.response import TemplateResponse
 from .models import MaintenanceDevice, Maintenance
-
+from pyscada.hmi.models import Page
 
 logger = logging.getLogger(__name__)
 
@@ -24,18 +24,24 @@ def unauthenticated_redirect(func):
 
 
 @unauthenticated_redirect
-def device_informations(request, device_id):
+def device_informations(request, device_id, link_title):
     
     device = MaintenanceDevice.objects.get(pk=device_id)
     maintenances = Maintenance.objects.filter(maintenanceDevice_id=device_id)
     fields = device._meta.get_fields() 
     
+    page_list = Page.objects.all()
+    
     context={
         "device": device,
         "maintenances": maintenances,
-        "fields": fields
+        "fields": fields,
+        "page_list": page_list,
+        "link_title": link_title
     }
     
     return TemplateResponse(
         request, "device-maintenance-list.html", context=context
     )
+    
+    
